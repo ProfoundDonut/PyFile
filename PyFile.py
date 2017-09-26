a8f74261b81a74e0f3bcd902b9bc7a05a20d668e
shadyenapp <hayden.sapp@gmail.com>
2017-09-26 08:50:18 -0600

    I deleted my sh**

--- PyFile.py
+++ PyFile.py
@@ -1,30 +1,4 @@
-""" Hayden's Shit
-#What is this going to be about?
-def loop():
-    print("Authorized user...")
-    print("...")
-    print("Loading PyShell...")
-    print("...")
-    print("")
-    print("")
-    print("")
-    print("")
-    print("")
-    print("")
-    print("")
-    print("")
-    print("Welcome to PyShell!")
-    lup = True
-    while loop == True:
-        var = input(name + "--$> ")
-        if var == "help"
 
-        else:
-            print("That is not a command. Please type 'help' for help.")
-name = input("What is your name? \n")
-if name.lower() == "hayden" or name.lower() == "max" or name.lower() == "hunter":
-    loop()
-"""
 import csv
 import os
 import sys
