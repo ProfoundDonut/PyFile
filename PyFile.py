5d6f27bae7b0511fa1a64ea7325bc51b843a3960
shadyenapp <hayden.sapp@gmail.com>
2017-09-26 08:32:39 -0600

    added things

--- PyFile.py
+++ PyFile.py
@@ -1,7 +1,25 @@
 #What is this going to be about?
 def loop():
+    print("Authorized user...")
+    print("...")
+    print("Loading PyShell...")
+    print("...")
+    print("")
+    print("")
+    print("")
+    print("")
+    print("")
+    print("")
+    print("")
+    print("")
+    print("Welcome to PyShell!")
+    lup = True
+    while loop == True:
+        var = input(name + "--$> ")
+        if var == "help"
 
-    
+        else:
+            print("That is not a command. Please type 'help' for help.")
 name = input("What is your name? \n")
 if name.lower() == "hayden" or name.lower() == "max" or name.lower() == "hunter":
     loop()
